1ae880125aee48c29584bada8f9cd046e2220e36
Yann <yann@dechamplain.net>
2026-06-19 10:08:46 -0400

    fix: corriger l'enregistrement du Service Worker (redirection sw.js)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import withSerwist from "@serwist/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSerwist({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+})(nextConfig);
